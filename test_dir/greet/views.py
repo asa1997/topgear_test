9fcb4ce749cbf175dd64b959a43e640fbce3238b
jobyko <root@ip-172-31-35-38.ap-south-1.compute.internal>
2022-02-16 09:07:41 +0000

    changed added 1

--- test_dir/greet/views.py
+++ test_dir/greet/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello(request):
-    return HttpResponse("hello world - updated - gcp pipeline test")
+    return HttpResponse("GCP pipeline test - set image ")
     
