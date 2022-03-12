d08ec8bde62c67149802a866a0ac0570f6dfb955
jobyko <joby.ouseph@wipro.com>
2022-03-12 03:36:08 +0000

    test

--- test_repo/greet/views.py
+++ test_repo/greet/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello(request):
-    return HttpResponse("hello world - updated - django aws (from jenkins) testing")
+    return HttpResponse("hello world - updated - django aws + jenkins testing")
     
